cf1889f4f1ca2d288e9cc21b52e97512201b62fc
Mahesh <laxmanrathod1695@gmail.com>
2024-07-07 15:29:40 +0530

    This is a combination of 2 commits.

---
+++ src/Array/practiceArray.java
@@ -0,0 +1,9 @@
+package Array;
+
+public class practiceArray {
+    public  static void main(String[]args){
+
+        //you can practice more example
+    }
+
+}
---
+++ src/ArrayListAddMethod/practiceArrayListMethod.java
@@ -0,0 +1,9 @@
+package ArrayListAddMethod;
+
+public class practiceArrayListMethod {
+
+    public static void main(String []args){
+        // You can practice more example of ArrayListMethod
+    }
+
+}
