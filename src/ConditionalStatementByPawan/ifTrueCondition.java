2f123e08b5962119139ca263efef56855429b50f
Mahesh <laxmanrathod1695@gmail.com>
2024-07-07 09:02:39 +0530

    If condition Program

---
+++ src/ConditionalStatementByPawan/ifTrueCondition.java
@@ -0,0 +1,12 @@
+package ConditionalStatementByPawan;
+
+public class ifTrueCondition {
+
+    public static void main(String[]args){
+        if(true){
+            System.out.println("Condition is true");
+        }else {
+            System.out.println("Condition is false");
+        }
+    }
+}
