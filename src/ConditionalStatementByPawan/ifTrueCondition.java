94109eb18708241215dd9dc3e26ebb3e3223866e
Mahesh <laxmanrathod1695@gmail.com>
2024-07-07 11:49:20 +0530

    new cut branch changes

--- src/ConditionalStatementByPawan/ifTrueCondition.java
+++ src/ConditionalStatementByPawan/ifTrueCondition.java
@@ -4,7 +4,7 @@ public class ifTrueCondition {
 
     public static void main(String[]args){
         // if we make condition false it will go in else block 
-        if(false){
+        if(true){
             System.out.println("Condition is true");
         }else {
             System.out.println("Condition is false");
