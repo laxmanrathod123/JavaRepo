a86104cd92d41f245687b96b2c5844655c0930c1
Mahesh <laxmanrathod1695@gmail.com>
2024-07-07 09:33:12 +0530

    Review changes addressed

--- src/ConditionalStatementByPawan/ifTrueCondition.java
+++ src/ConditionalStatementByPawan/ifTrueCondition.java
@@ -3,7 +3,8 @@ package ConditionalStatementByPawan;
 public class ifTrueCondition {
 
     public static void main(String[]args){
-        if(true){
+        // if we make condition false it will go in else block 
+        if(false){
             System.out.println("Condition is true");
         }else {
             System.out.println("Condition is false");
